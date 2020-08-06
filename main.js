032f14328599836e673dced63ece78f537c94ad0
Ben Paran Rutterford <ben.p.rutterford@gmail.com>
2020-08-06 23:15:32 +0100

    updated main.js

--- main.js
+++ main.js
@@ -5,9 +5,24 @@ const url = require('url');
 let win;
 
 function createWindow(){
+    // Create browser window
     win = new BrowserWindow({width:800, height:800, icon:__dirname+'/img/logo temp.png'});
     
+    // Load index.html
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
     }));
+
+    // Open devtools
+    win.webContents().openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+
+    });
 }
+
+// Run create window function
+app.on('ready', createWindow)
